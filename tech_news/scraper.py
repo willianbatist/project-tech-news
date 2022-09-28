96d5e4476d975269566a3fea43b59c504d4a529c
Willian <willian.alves.b15@gmail.com>
2022-09-28 18:53:14 -0300

    feat: req 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+            )
+        if response.status_code == 200:
+            return response.text
+        if response.status_code != 200:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
